5f09be69efd22c50d4be165b0fdad5e80714d4d0
limsol02 <yesshol@naver.com>
2024-07-29 00:21:28 +0900

    # 컨트롤러 생성

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // view를 리턴
 public class IndexController {
-    
+    // 컨트롤러    
     // localhost:8080/
     // localhost:8080
     @GetMapping({"","/"})
