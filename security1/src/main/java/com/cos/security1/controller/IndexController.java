09441f1a6ae1fd26cbca7682b4be98b99512168f
limsol02 <yesshol@naver.com>
2024-07-29 00:17:50 +0900

    # Spring security 초기설정

---
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // view를 리턴
+public class IndexController {
+    
+    // localhost:8080/
+    // localhost:8080
+    @GetMapping({"","/"})
+    // 머스테치 :
+    public String index(){
+        return "index";
+    }
+}
