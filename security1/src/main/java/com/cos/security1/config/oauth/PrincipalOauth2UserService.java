dc0eb83ba24893e56f4a68045b4926aa26900089
sol lim <yesshol@naver.com>
2024-08-19 13:06:11 +0900

    # springsecurity 네이버 로그인

--- security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.cos.security1.config.oauth;
 import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.config.oauth.provider.FacebookUserInfo;
 import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
 import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service // 구글 로그인 이후 후처리 클래스
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -44,12 +47,17 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         OAuth2UserInfo oAuth2UserInfo = null;
         if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
             System.out.println("구글 로그인 요청");
-            oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+            oAuth2UserInfo =  new GoogleUserInfo(oauth2User.getAttributes());
         }else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
             System.out.println("페이스북 로그인 요청");
-            oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
-        }else{
-            System.out.println("우리는 구글과 페이스북만 지원하고있습니다.");
+            oAuth2UserInfo =  new FacebookUserInfo(oauth2User.getAttributes());
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("네이버 로그인 요청");
+            // naver는 response 로 정보를 받아오기 때문에
+            oAuth2UserInfo =  new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
+        }
+        else{
+            System.out.println("우리는 구글과 페이스북, 네이버만 지원하고있습니다.");
         }
 
         String provider = oAuth2UserInfo.getProvider(); // 회사명
@@ -61,6 +69,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
         User userEntity = userRepository.findByUsername(username);
         if(userEntity==null){
+            System.out.println("소셜 로그인이 최초입니다.");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
